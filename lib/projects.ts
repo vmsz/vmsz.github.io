0514b725ec3bcf503bdb7fd144ef2369a7ece0a3
vmsz <vmsz.dev@gmail.com>
2026-06-23 20:45:05 -0300

    feat: updating personal projects

--- lib/projects.ts
+++ lib/projects.ts
@@ -4,14 +4,14 @@ import { FaTrowelBricks } from 'react-icons/fa6';
 const projects: Project[] = [
   {
     icon: FaTrowelBricks,
-    title: 'Software Orso',
+    title: 'Civio',
     beginDate: '11/2024',
     endDate: '02/2025',
-    subtitle: 'Software orçamentista para obras de construção civil.',
+    subtitle: 'Software orçamentista para obras de construção civil',
     description:
       'Contém um algoritmo de guias, salvar e carregar progresso, exportação em PDF e Excel, editor de composições e BDI, itenização automática de todas as tarefas.',
     skills: ['react', 'javascript', 'tailwindcss', 'nodejs', 'postgresql'],
-    codeHref: 'https://github.com/vmsz/software-orso',
+    codeHref: 'https://github.com/vmsz/civio',
   },
 ];
 
